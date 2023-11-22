4961c03982984c16ca0057e0432d8edffc90f2be
amirmahdy <amir.malayjerdi@gmail.com>
2023-11-22 10:10:00 +0330

    add gRPC gateway

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module main
 
 go 1.21.4
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	google.golang.org/grpc v1.59.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"api"
+	"context"
 	"database/sql"
 	"db"
 	"gapi"
 	"log"
 	"net"
+	"net/http"
 	"pb"
 	"utils"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -27,6 +30,7 @@ func main() {
 	}
 	store := db.NewSQLStore(conn)
 	// runGinServer(config, store)
+	go runGatewayServer(config, store)
 	runGRPCServer(config, store)
 
 }
@@ -53,6 +57,34 @@ func runGRPCServer(config utils.Config, store db.Store) {
 		log.Fatal("cannot listen to grpc", err)
 	}
 	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
+
+func runGatewayServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+	grpcMux := runtime.NewServeMux()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterCoinGateHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddrress)
+	if err != nil {
+		log.Fatal("cannot listen to grpc", err)
+	}
+
+	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
